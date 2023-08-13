9cdc8d2096488209c36072b46b4b788a62da38ca
root <root@DESKTOP-5P8384K.localdomain>
2023-08-13 20:21:27 +0300

    Add some tests

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,6 +16,9 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "a", expected: "a"},
+		{input: "a2a", expected: "aaa"},
+		{input: "a0", expected: ""},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
